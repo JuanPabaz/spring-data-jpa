e5c4b6549277fd5af66e087bb63cd32bb65441dd
JuanPabaz <jpgthf@gmail.com>
2024-08-04 15:15:25 -0500

    feat(Pizza): add getById endpoint

--- src/main/java/com/pizzeria/controller/PizzaController.java
+++ src/main/java/com/pizzeria/controller/PizzaController.java
@@ -1,10 +1,10 @@
 package com.pizzeria.controller;
 
 import com.pizzeria.service.PizzaService;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +18,11 @@ public class PizzaController {
     @GetMapping
     public ResponseEntity<?> getAll(){
         return ResponseEntity.ok(pizzaService.getAll());
+    }
+
+    @GetMapping("/{idPizza}")
+    public ResponseEntity<?> getById(@PathVariable(name = "idPizza")Integer idPizza) throws Exception {
+        return ResponseEntity.ok(pizzaService.getById(idPizza));
     }
 
 }
