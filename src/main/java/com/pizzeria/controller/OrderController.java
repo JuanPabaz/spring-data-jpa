32a0b1d55a8634b5bb872463d49218091a7e116b
JuanPabaz <jpgthf@gmail.com>
2024-08-04 18:00:59 -0500

    feat(Order): add OrderController

---
+++ src/main/java/com/pizzeria/controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.pizzeria.controller;
+
+import com.pizzeria.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping
+    public ResponseEntity<?> getAll() throws Exception {
+        return ResponseEntity.ok(orderService.getAll());
+    }
+
+}
