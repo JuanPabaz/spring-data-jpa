b88f833d5c31948fd83b317d1005e12f82e44998
JuanPabaz <jpgthf@gmail.com>
2024-08-20 17:41:55 -0500

    feat(Pizza): Add findByAvailableTrue

--- src/main/java/com/pizzeria/persistence/repository/PizzaPagSortRepository.java
+++ src/main/java/com/pizzeria/persistence/repository/PizzaPagSortRepository.java
@@ -1,7 +1,12 @@
 package com.pizzeria.persistence.repository;
 
 import com.pizzeria.persistence.entity.Pizza;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.ListPagingAndSortingRepository;
 
 public interface PizzaPagSortRepository extends ListPagingAndSortingRepository<Pizza, Integer> {
+
+    Page<Pizza> findByAvailableTrue(Pageable pageable);
+
 }
