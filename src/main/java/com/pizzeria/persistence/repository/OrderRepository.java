42c8f8ec3b405c551e98c6dc4dcce630953a30ff
JuanPabaz <jpgthf@gmail.com>
2024-08-20 17:09:18 -0500

    feat(Order): add findByDate and getOutsideOrder querys

--- src/main/java/com/pizzeria/persistence/repository/OrderRepository.java
+++ src/main/java/com/pizzeria/persistence/repository/OrderRepository.java
@@ -3,5 +3,12 @@ package com.pizzeria.persistence.repository;
 import com.pizzeria.persistence.entity.Order;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface OrderRepository extends ListCrudRepository<Order, Integer> {
+
+    List<Order> findAllByDateAfter(LocalDate date);
+    List<Order> findAllByMethodIn(List<String> methods);
+
 }
