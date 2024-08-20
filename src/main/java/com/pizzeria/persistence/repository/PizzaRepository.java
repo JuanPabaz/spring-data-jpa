aa651293efa4f963da9212c0ec3b98fa6d58dd8b
JuanPabaz <jpgthf@gmail.com>
2024-08-20 17:10:13 -0500

    feat(Pizza): change return type in findFirstByAvailableTrueAndNameIgnoreCase and add find the 3 cheapest pizza

--- src/main/java/com/pizzeria/persistence/repository/PizzaRepository.java
+++ src/main/java/com/pizzeria/persistence/repository/PizzaRepository.java
@@ -4,15 +4,19 @@ import com.pizzeria.persistence.entity.Pizza;
 import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PizzaRepository extends ListCrudRepository<Pizza, Integer> {
 
     List<Pizza> findAllByAvailableTrueOrderByPrice();
 
-    Pizza findAllByAvailableTrueAndNameIgnoreCase(String name);
+    Optional<Pizza> findFirstByAvailableTrueAndNameIgnoreCase(String name);
 
     List<Pizza> findAllByAvailableTrueAndDescriptionContainingIgnoreCase(String description);
 
     List<Pizza> findAllByAvailableTrueAndDescriptionNotContainingIgnoreCase(String description);
+    List<Pizza> findTop3ByAvailableTrueAndPriceLessThanEqualOrderByPriceAsc(Double price);
+
+    int countByVeganTrue();
 
 }
