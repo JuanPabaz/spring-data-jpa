6329cb29dac0f506fd3b24d2fda5ef75e10edbf5
JuanPabaz <jpgthf@gmail.com>
2024-08-20 18:02:25 -0500

    feat(Order): add findCustomerOrders native query

--- src/main/java/com/pizzeria/persistence/repository/OrderRepository.java
+++ src/main/java/com/pizzeria/persistence/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.pizzeria.persistence.repository;
 
 import com.pizzeria.persistence.entity.Order;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
 import java.time.LocalDate;
@@ -10,5 +11,8 @@ public interface OrderRepository extends ListCrudRepository<Order, Integer> {
 
     List<Order> findAllByDateAfter(LocalDate date);
     List<Order> findAllByMethodIn(List<String> methods);
+
+    @Query(value = "SELECT * FROM pizza_order WHERE id_customer = :idCustomer", nativeQuery = true)
+    List<Order> findCustomerOrders(String idCustomer);
 
 }
