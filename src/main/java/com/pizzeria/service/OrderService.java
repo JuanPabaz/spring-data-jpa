59c26e9277e8d5b4b33ccbde1f7963e25cdfbed0
JuanPabaz <jpgthf@gmail.com>
2024-08-20 17:09:28 -0500

    feat(Order): add findByDate and getOutsideOrder methods

--- src/main/java/com/pizzeria/service/OrderService.java
+++ src/main/java/com/pizzeria/service/OrderService.java
@@ -5,10 +5,16 @@ import com.pizzeria.persistence.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class OrderService {
+
+    private static final String DELIVERY = "D";
+    private static final String CARRYOUT = "C";
+    private static final String ON_SITE = "S";
 
     @Autowired
     private OrderRepository orderRepository;
@@ -16,6 +22,24 @@ public class OrderService {
     public List<Order> getAll() throws Exception {
         try {
             return orderRepository.findAll();
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public List<Order> findByDate() throws Exception {
+        try {
+            LocalDate today = LocalDate.from(LocalDate.now().atTime(0, 0));
+            return orderRepository.findAllByDateAfter(today);
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public List<Order> getOutsideOrders() throws Exception {
+        try {
+            List<String> methods = Arrays.asList(DELIVERY, CARRYOUT);
+            return this.orderRepository.findAllByMethodIn(methods);
         }catch (Exception e){
             throw new Exception(e.getMessage());
         }
